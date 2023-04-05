ad128f12e40a9151088d8fcb49473065be20b42b
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2023-04-05 13:06:50 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/biensScannes',[BiensScannesController::class,'index']);
+ Route::get('/biensScannes',[BiensScannesController::class,'index']); //GetAllBiensScannes
