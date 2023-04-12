0872fe7e260dade4db23ca1797510ccde6f99e65
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2023-04-12 17:56:07 +0100

    LoginFunction

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\DemandeCompte;
 use App\Models\role;
 use App\Models\User;
+use App\Models\Centre;
+use App\Models\Unite;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+       //*REGISTRATION //
     public function register(Request $request){
 
         $validator = Validator::make($request->all(), [
@@ -19,7 +23,7 @@ class AuthController extends Controller
             'matricule' => 'required|string|unique:users',
             'password' => 'required|string|min:8',
             'role' => 'required|string|in:Admin,Chef_équipe,Chef_centre,Chef_unité',
-            'structure_type' => 'required|string|in:Centre,Unite',
+            'structure_type' => 'required|string',
             'structure_id' => 'required|string',
 
         ]);
@@ -31,21 +35,29 @@ class AuthController extends Controller
         $role = Role::where('name', $request->role)->firstOrFail();
 
         // Retrieve the structure based on its type and ID
-        $structureType = $request->structure_type;
-        $structureId = $request->structure_id;
-        $structure = $structureType::find($structureId);
+        if ($request->structure_type === 'Centre') {
+            $structureType = 'App\Models\Centre';
+        } elseif ($request->structure_type === 'Unite') {
+            $structureType = 'App\Models\Unite';
+        } else {
+            return response()->json(['message' => 'Invalid structure type'], 422);
+        }
+
+        $structure = $structureType::find($request->structure_id);
         if ($structure === null) {
             return response()->json(['message' => 'Structure not found'], 404);
         }
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
             'matricule' => $request->matricule,
             'role_id' => $role->id,
-            'structure_type' => get_class($structure),
+            // 'structure_type' => get_class($structure),
+            'structure_type' => $structureType,
             'structure_id' => $structure->getKey(),
-            
+
         ]);
 
          // Create a new account demande for the user
@@ -57,6 +69,45 @@ class AuthController extends Controller
         return response()->json(['message' => 'User created successfully', 'user' => $user,'demandeCompte'=>$demandeCompte]);
    }
 
+   //*LOGIN //
+   public function login(Request $request){
+    $validator = Validator::make($request->all(),
+    [   'matricule' => 'required|matricule',
+        'password' => 'required'
+    ]);
+    if ($validator->fails()) {
+        return response()->json(['message' => 'validation error','errors' => $validator->errors()], 422);
+    }
 
+    $user= User::where('matricule',$request->matricule)->first();
+    if (!$user) {
+        return response()->json([
+            'status' => false,
+            'message' => 'User not found.',
+        ], 401);
+    }
+
+    if (!$user->Compte_isActivated) {
+        return response()->json(['message' => 'Your account has not been activated yet'], 401);
+    }
+
+    $demandeCompte = $user->demandeCompte;
+    if (!$demandeCompte || $demandeCompte->status !== 'accepted') {
+        return response()->json(['message' => 'Your Account has not been accepted yet'], 401);
+    }
+
+    if (!Auth::attempt($request->only(['matricule', 'password']))) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    $token=$user->createToken('myapptoken')->plainTextToken;
+    $response = [
+        'message' => 'User Logged In Successfully',
+        'user' => $user,
+        'token'=> $token
+    ];
+    return response($response,201);
+
+}
 
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('matricule')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('Compte_isActivated')->default(false);
-            $table->unsignedBigInteger('role_id');
+            $table->Integer('role_id');
             $table->morphs('structure');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
 
