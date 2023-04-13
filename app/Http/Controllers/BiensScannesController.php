4206363f99a8db18d8c8feb28a8b949c2512dcab
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2023-04-13 11:48:35 +0100

    Create listeInventairesScannés filtered by role

--- app/Http/Controllers/BiensScannesController.php
+++ app/Http/Controllers/BiensScannesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\BiensScannes;
 use App\Models\Localite;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BiensScannesController extends Controller
 {
@@ -13,7 +14,7 @@ class BiensScannesController extends Controller
     {
         return BiensScannes::all();
     }
-
+// * les localités visités et non visités
     public function localitesVisites(){
         $visitedLocalities =Localite::has('biensScannes')
         ->select('LOC_ID', 'LOC_LIB')
@@ -27,5 +28,27 @@ class BiensScannesController extends Controller
         return response()->json($NotvisitedLocalities);
     }
 
-
+//* Filtrer liste d'inventaires par structure
+public function listeInventairesScannés()
+{
+    $user = Auth::user();
+    switch ($user->role_id) {
+        case '1':
+            $scannedInventory = BiensScannes::where('UCM_ID', $user->structure_id)->get();
+            break;
+        case '2':
+            $scannedInventory = BiensScannes::where('COP_ID', $user->structure_id)->get();
+            break;
+        case '3':
+            $scannedInventory = BiensScannes::whereIn('LOC_ID', function($query) use ($user) {
+                        $query->select('LOC_ID')
+                              ->from('BienScannés')
+                              ->where('EMP_ID', $user->matricule)
+                              ->where('EMP_IS_MANAGER', 1)
+                              ->groupBy('LOC_ID');
+                    })->get();
+                    break;
+}
+     return response()->json(['inventoryList' => $scannedInventory], 200);
+}
 }
