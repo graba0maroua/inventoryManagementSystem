c9a7580b47796643db4b81bcb39801844cf1b98e
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2023-04-24 14:24:37 +0100

    update

--- app/Http/Controllers/BiensScannesController.php
+++ app/Http/Controllers/BiensScannesController.php
@@ -69,42 +69,4 @@ public function listeInventairesScannes(Request $request)
 
 
 
-
-public function countScannedInventoryByCenter()
-{
-    $counts = DB::table('INV.T_R_CENTRE_OPERATIONNEL_COP')
-        ->leftJoin('INV.T_BIENS_SCANNES', 'INV.T_R_CENTRE_OPERATIONNEL_COP.COP_ID', '=', 'INV.T_BIENS_SCANNES.COP_ID')
-        ->select('INV.T_R_CENTRE_OPERATIONNEL_COP.COP_ID', 'INV.T_R_CENTRE_OPERATIONNEL_COP.COP_LIB', DB::raw('COUNT(DISTINCT INV.T_BIENS_SCANNES.code_bar) as scanned_count'))
-        ->groupBy('INV.T_R_CENTRE_OPERATIONNEL_COP.COP_ID', 'INV.T_R_CENTRE_OPERATIONNEL_COP.COP_LIB')
-        ->get();
-
-    return response()->json($counts);
-}
-public function countScannedInventoryByLocality()
-{
-    $counts = Localite::withCount(['biensScannes as scanned_count' => function ($query) {
-            $query->select(DB::raw('COUNT(DISTINCT code_bar)'));
-        }])
-        ->get(['LOC_ID', 'COP_ID']);
-
-    return response()->json($counts);
-}
-public function countScannedInventoryByLocalite()
-    {
-        $localites = Localite::withCount('biensScannes')->get();
-
-        $result = [];
-        foreach ($localites as $localite) {
-            $count = $localite->biens_scannes_count;
-            $result[] = [
-                'localite_id' => $localite->LOC_ID,
-                'localite_name' => $localite->LOC_LIB,
-                'count' => $count,
-            ];
-        }
-
-        return $result;
-    }
-
-
 }
