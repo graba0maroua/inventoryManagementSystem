8fc648287a00ec32b0665d2bf2f0a007207235c9
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2023-04-25 13:18:37 +0100

    update

--- app/Models/Equipe.php
+++ app/Models/Equipe.php
@@ -14,6 +14,6 @@ class Equipe extends Model
     public $incrementing = false;
     public function localites()
     {
-        return $this->belongsToMany(Localite::class, 'equipe_localite', 'GROUPE_ID', 'LOC_ID');
+        return $this->belongsToMany(Localite::class);
     }
 }
